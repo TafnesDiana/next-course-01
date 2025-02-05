dafa15b19611bdfdd4fcc192621d33134a842054
Tafnes Diana <tafnessantos@natura.net>
2025-02-05 16:57:06 -0300

    Course initialization

---
+++ src/app/component/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="bg-slate-100 text-center justify-center py-10 font-bold">
+        <p>Curso de Next.js &copy; 2025</p>
+      </footer>
+    </>
+  );
+}
---
+++ src/app/products/pants/page.tsx
@@ -0,0 +1,3 @@
+export default function Pants(){
+    return <h1>Page Pantssssssssssssssssssssssssssssssssssssssssss</h1>
+}
